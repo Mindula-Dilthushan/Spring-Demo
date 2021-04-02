90d14e287b92007376168a0805464a4bb4a3373c
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-04-02 09:01:48 +0530

    DB Connection Instantiate class

---
+++ module/01_Hello_Spring/src/lk/bean/DBConnection.java
@@ -0,0 +1,8 @@
+package lk.bean;
+
+public class DBConnection {
+
+    public DBConnection() {
+        System.out.println("DB Connection Instantiate");
+    }
+}
--- module/01_Hello_Spring/src/lk/config/AppConfig.java
+++ module/01_Hello_Spring/src/lk/config/AppConfig.java
@@ -1,9 +1,16 @@
 package lk.config;
 
+import lk.bean.DBConnection;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "lk")
 public class AppConfig {
+
+    @Bean
+    public DBConnection dbConnection(){
+        return new DBConnection();
+    }
 }
