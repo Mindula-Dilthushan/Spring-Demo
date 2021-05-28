7a78548e22b88116a8f706a57943d1e487119819
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-28 18:34:11 +0530

    Customer Controller Class Create

--- module/09_Maven_01/src/main/java/lk/config/WebAppConfig.java
+++ module/09_Maven_01/src/main/java/lk/config/WebAppConfig.java
@@ -1,4 +1,11 @@
 package lk.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk")
 public class WebAppConfig {
 }
---
+++ module/09_Maven_01/src/main/java/lk/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package lk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    @GetMapping
+    public String getCustomer(){
+        return "Get Customer";
+    }
+}
