db50f0bb9efd353c970f126b718abcd795583a79
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-01 15:17:58 +0530

    Customer Entity Create

--- module/11_Spring__Data_JPA/src/main/java/lk/config/JPAConfig.java
+++ module/11_Spring__Data_JPA/src/main/java/lk/config/JPAConfig.java
@@ -2,14 +2,42 @@ package lk.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import javax.sql.DataSource;
 
 @Configuration
 public class JPAConfig {
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,JpaVendorAdapter vendorAdapter){
+
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
         return factoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_demo?createDatabaseIfNotExist==true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1023");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter vendorAdapter(){
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        vendorAdapter.setDatabase(Database.MYSQL);
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(true);
+        return vendorAdapter;
     }
 }
