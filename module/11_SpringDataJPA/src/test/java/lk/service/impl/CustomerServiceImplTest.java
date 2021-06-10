4d3f7b9c3427a093afba688ab012cc48317a8629
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-10 21:10:21 +0530

    get all test class create

---
+++ module/11_SpringDataJPA/src/test/java/lk/service/impl/CustomerServiceImplTest.java
@@ -0,0 +1,11 @@
+package lk.service.impl;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceImplTest {
+
+    @Test
+    void getAllCustomers() {
+    }
+}
